86f5d49282270e3ecc13f25644ea40dffd5f2aef
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2020-12-30 23:27:35 +0530

    export modules

---
+++ progressive_gan/model/__init__.py
@@ -0,0 +1,11 @@
+from progressive_gan.model.modules.modules_impl import (
+    DiscriminatorDownsampleBlock, DiscriminatorFinalBlock, GeneratorBaseBlock,
+    GeneratorUpsampleBlock, ToRGBBlock)
+
+__all__ = [
+    'DiscriminatorDownsampleBlock',
+    'DiscriminatorFinalBlock',
+    'GeneratorBaseBlock',
+    'GeneratorUpsampleBlock',
+    'ToRGBBlock'
+]
---
+++ progressive_gan/model/networks.py
@@ -0,0 +1,62 @@
+import numpy as np
+import tensorflow as tf
+
+from progressive_gan.model import (
+    DiscriminatorDownsampleBlock, DiscriminatorFinalBlock, GeneratorBaseBlock,
+    GeneratorUpsampleBlock, ToRGBBlock)
+
+
+class Generator(tf.keras.Model):
+
+    def __init__(self, max_resolution, use_equalized_layers, **kwargs):
+        super(Generator, self).__init__(**kwargs)
+
+        self.min_depth = 2
+        self.current_depth = 2
+        self.max_resolution = max_resolution
+        self.max_depth = int(np.log2(max_resolution))
+        self.use_equalized_layers = use_equalized_layers
+
+        self.blocks = {
+            2:
+                GeneratorBaseBlock(filters=Generator._nf(stage=1),
+                                   use_equalized_layers=use_equalized_layers,
+                                   name='depth-2')
+        }
+
+        for stage in range(2, self.max_depth):
+            self.blocks[stage + 1] = GeneratorUpsampleBlock(
+                filters=Generator._nf(stage=stage),
+                use_equalized_layers=use_equalized_layers,
+                name='depth-{}'.format(stage + 1))
+
+        self.upscale_2x = tf.keras.layers.UpSampling2D(size=2,
+                                                       interpolation='nearest')
+
+        self.to_rgb_blocks = {
+            i + 1: ToRGBBlock(use_equalized_layers=use_equalized_layers,
+                              name='depth-{}'.format(i + 1))
+            for i in range(1, self.max_depth)
+        }
+
+    def call(self, x):
+        noise, alpha = x
+        y = noise
+
+        if self.current_depth == self.min_depth:
+            y = self.blocks[self.current_depth](y)
+            return self.to_rgb_blocks[self.current_depth](y)
+
+        for depth in range(self.min_depth, self.current_depth):
+            y = self.blocks[depth](y)
+
+        residual = \
+            self.upscale_2x(self.to_rgb_blocks[self.current_depth - 1](y))
+
+        straight = self.blocks[self.current_depth](y)
+        straight = self.to_rgb_blocks[self.current_depth](straight)
+        return (1 - alpha) * residual + alpha * straight
+
+    @staticmethod
+    def _nf(stage, fmap_base=8192, fmap_max=512, fmap_decay=1.0):
+        return min(int(fmap_base / (2.0**(stage * fmap_decay))), fmap_max)
