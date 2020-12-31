f649bca9ea3438c1fee4bc669389e89dee1d3d86
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-01 04:11:03 +0530

    implemented Discriminator

--- progressive_gan/model/networks.py
+++ progressive_gan/model/networks.py
@@ -2,9 +2,10 @@ import numpy as np
 import tensorflow as tf
 from absl import logging
 
-from progressive_gan.model import (
-    DiscriminatorDownsampleBlock, DiscriminatorFinalBlock, GeneratorBaseBlock,
-    GeneratorUpsampleBlock, ToRGBBlock)
+from progressive_gan.model import (DiscriminatorDownsampleBlock,
+                                   DiscriminatorFinalBlock, FromRGBBlock,
+                                   GeneratorBaseBlock, GeneratorUpsampleBlock,
+                                   ToRGBBlock)
 
 
 class Generator(tf.keras.Model):
@@ -62,6 +63,97 @@ class Generator(tf.keras.Model):
         straight = self.blocks[str(self.current_depth)](y)
         straight = self.to_rgb_blocks[str(self.current_depth)](straight)
         return (1 - alpha) * residual + alpha * straight
+
+    @staticmethod
+    def _nf(stage, fmap_base=8192, fmap_max=512, fmap_decay=1.0):
+        return min(int(fmap_base / (2.0**(stage * fmap_decay))), fmap_max)
+
+    def assign_depth(self, depth):
+        depth = int(depth)
+        if depth != self.current_depth:
+            logging.info('Changing depth from {} to {}'.format(
+                self.current_depth, depth))
+            self.current_depth = depth
+            self._current_depth.assign(depth)
+
+    def increment_depth(self):
+        assert self.current_depth != self.max_depth + 1, 'Max Depth Exceeded'
+        logging.info('Incrementing depth from {} to {}'.format(
+            self.current_depth, self.current_depth + 1))
+        self.current_depth += 1
+        self._current_depth.assign_add(1)
+
+    def restore_current_depth(self):
+        depth = self._current_depth.numpy()
+        if depth != self.current_depth:
+            logging.info('Changing depth from {} to {}'.format(
+                self.current_depth, depth))
+            self.current_depth = depth
+
+
+class Discriminator(tf.keras.Model):
+
+    def __init__(self, max_resolution, use_equalized_layers, **kwargs):
+        super(Discriminator, self).__init__(**kwargs)
+
+        self.min_depth = 2
+        self.current_depth = 2
+        self.max_resolution = max_resolution
+        self.max_depth = int(np.log2(max_resolution))
+        self.use_equalized_layers = use_equalized_layers
+
+        self.blocks = {
+            '2':
+                DiscriminatorFinalBlock(
+                    filters=Discriminator._nf(stage=1),
+                    use_equalized_layers=use_equalized_layers,
+                    name='depth-2-conv-block')
+        }
+
+        for stage in range(2, self.max_depth):
+            self.blocks[str(stage + 1)] = DiscriminatorDownsampleBlock(
+                filters=[
+                    Discriminator._nf(stage=stage),
+                    Discriminator._nf(stage=stage - 1)
+                ],
+                use_equalized_layers=use_equalized_layers,
+                name='depth-{}-conv-block'.format(stage + 1))
+
+        self.downscale_2x = tf.keras.layers.AvgPool2D(
+            pool_size=2, name='avgpool2d-2x-downsampling')
+
+        self.from_rgb_blocks = {
+            str(i + 1): FromRGBBlock(filters=Discriminator._nf(stage=i),
+                                     use_equalized_layers=use_equalized_layers,
+                                     name='depth-{}-from-rgb'.format(i + 1))
+            for i in range(1, self.max_depth)
+        }
+
+        self._current_depth = tf.Variable(self.current_depth,
+                                          trainable=False,
+                                          name='current_depth',
+                                          dtype=tf.uint8)
+
+    def call(self, x):
+        images, alpha = x
+        y = images
+
+        if self.current_depth == self.min_depth:
+            y = self.from_rgb_blocks[str(self.current_depth)](y)
+            return self.blocks[str(self.current_depth)](y)
+
+        residual = self.downscale_2x(y)
+        residual = self.from_rgb_blocks[str(self.current_depth - 1)](residual)
+
+        straight = self.from_rgb_blocks[str(self.current_depth)](y)
+        straight = self.blocks[str(self.current_depth)](straight)
+
+        y = (1 - alpha) * residual + alpha * straight
+
+        for depth in range(self.current_depth - 1, self.min_depth - 1, -1):
+            y = self.blocks[str(depth)](y)
+
+        return y
 
     @staticmethod
     def _nf(stage, fmap_base=8192, fmap_max=512, fmap_decay=1.0):
