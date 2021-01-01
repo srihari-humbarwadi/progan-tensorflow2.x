0895ed09f37a69d1a900cdbf86b8ae5cba0d5437
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-02 00:56:44 +0530

    export InputPipeline, PreprocessingPipeline

---
+++ progressive_gan/dataloader/__init__.py
@@ -0,0 +1,5 @@
+from progressive_gan.dataloader.input_pipeline import InputPipeline
+from progressive_gan.dataloader.preprocessing_pipeline import \
+    PreprocessingPipeline
+
+__all__ = ['InputPipeline', 'PreprocessingPipeline']
