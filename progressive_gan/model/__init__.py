059989c615dbbcc0d27d6fbb3296b4692932fcaf
srihari-humbarwadi <sriharihumbarwadi97@gmail.com>
2021-01-02 00:58:51 +0530

    export Generator, Discriminator

--- progressive_gan/model/__init__.py
+++ progressive_gan/model/__init__.py
@@ -1,10 +1,15 @@
 from progressive_gan.model.modules.modules_impl import (
     DiscriminatorDownsampleBlock, DiscriminatorFinalBlock, FromRGBBlock,
     GeneratorBaseBlock, GeneratorUpsampleBlock, ToRGBBlock)
+from progressive_gan.model.networks.base_network import BaseNetwork
+from progressive_gan.model.networks.networks import Discriminator, Generator
 
 __all__ = [
+    'BaseNetwork',
+    'Discriminator',
     'DiscriminatorDownsampleBlock',
     'DiscriminatorFinalBlock',
+    'Generator',
     'GeneratorBaseBlock',
     'GeneratorUpsampleBlock',
     'ToRGBBlock',
